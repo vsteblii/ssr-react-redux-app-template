6776a16d719f988a0e1c761b03ce2232ff769599
Volodymyr <volodymyr.steblii@gmail.com>
2020-12-12 16:20:18 +0200

    Create client side redux store

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import Routes from './Routes';
 
+const middlewares = [thunk];
+if (DEVELOPMENT) {
+    middlewares.push(createLogger());
+}
+
+const store = createStore(reducers, {}, applyMiddleware(middlewares));
+
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
