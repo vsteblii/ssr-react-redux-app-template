4fc0aa0dd36356ef1f01ebf805a5b665409f370a
Volodymyr <volodymyr.steblii@gmail.com>
2020-12-12 16:19:59 +0200

    Extend webpack config with define plugin - env

--- server/webpack.base.js
+++ server/webpack.base.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
     module: {
         rules: [
@@ -14,5 +17,13 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            PRODUCTION: JSON.stringify(isProduction),
+            DEVELOPMENT: JSON.stringify(!isProduction),
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
+        })
+    ],
 };
