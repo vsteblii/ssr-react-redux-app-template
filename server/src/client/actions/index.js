fd490defddc0b248c3d5e67cab42837d7d4309bf
Volodymyr <volodymyr.steblii@gmail.com>
2020-12-22 12:00:38 +0200

    Add anonymouse login action

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,10 +1,19 @@
 export const FETCH_USERS = 'fetch_users';
-
 export const fetchUsers = () => async (dispatch, getState, request) => {
     const res = await request.get('/api/v1/user/1');
 
     dispatch({
         type: FETCH_USERS,
+        payload: res,
+    });
+}
+
+export const ANONYMOUSE_LOGIN = 'anonymouse_login';
+export const anonymouseLogin = () => async (dispatch, getState, request) => {
+    const res = await request.get('/api/v1/login/anonymous');
+
+    dispatch({
+        type: ANONYMOUSE_LOGIN,
         payload: res,
     });
 }
